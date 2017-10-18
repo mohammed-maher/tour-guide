7993f729f47b56fb30ccdbaaa5727ce7be07df93
Muhammed Maher <maghmuh@gmail.com>
2017-10-18 12:35:49 +0300

    first update

--- app/src/main/java/com/re_coded/example/android/kirkukcity/LocationFragment.java
+++ app/src/main/java/com/re_coded/example/android/kirkukcity/LocationFragment.java
@@ -1,5 +1,6 @@
 package com.re_coded.example.android.kirkukcity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -13,56 +14,17 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link LocationFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link LocationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LocationFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    OnLocationSelectedListener mCallback;
 
-    private OnFragmentInteractionListener mListener;
+    // Container Activity must implement this interface
+    public interface OnLocationSelectedListener {
+        public void onLocationSelected(Context context,int position,ArrayList<Landmark> array);
+    }
 
     public LocationFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LocationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LocationFragment newInstance(String param1, String param2) {
-        LocationFragment fragment = new LocationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -85,56 +47,22 @@ public class LocationFragment extends Fragment {
         list.setOnItemClickListener(new ListView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView adapter, View v, int position, long lng){
-                Intent intent = new Intent(getActivity(),PlaceDetails.class);
-                intent.putExtra("TITLE",landmarks.get(position).getName());
-                intent.putExtra("IMAGE",landmarks.get(position).getImage());
-                intent.putExtra("DESCRIPTION",landmarks.get(position).getDescription());
-                intent.putExtra("ADDRESS",landmarks.get(position).getLocationAddress());
-                intent.putExtra("LONGITUDE",landmarks.get(position).getLocationLongitude());
-                intent.putExtra("LATITUDE",landmarks.get(position).getLocationLatitude());
-                v.getContext().startActivity(intent);
+            mCallback.onLocationSelected(v.getContext(),position,landmarks);
             }
 
         });
         return layout;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+    public void onAttach(Context activity) {
+        super.onAttach(activity);
+
+        try {
+            mCallback = (OnLocationSelectedListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnLocationSelectedListener");
         }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
--- app/src/main/java/com/re_coded/example/android/kirkukcity/MainActivity.java
+++ app/src/main/java/com/re_coded/example/android/kirkukcity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.re_coded.example.android.kirkukcity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +13,7 @@ import android.widget.ListView;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationFragment.OnLocationSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,4 +25,15 @@ public class MainActivity extends AppCompatActivity {
             .commit();
 }
 
+    @Override
+    public void onLocationSelected(Context context,int position,ArrayList<Landmark> landmarks) {
+        Intent intent = new Intent(context,PlaceDetails.class);
+        intent.putExtra("TITLE",landmarks.get(position).getName());
+        intent.putExtra("IMAGE",landmarks.get(position).getImage());
+        intent.putExtra("DESCRIPTION",landmarks.get(position).getDescription());
+        intent.putExtra("ADDRESS",landmarks.get(position).getLocationAddress());
+        intent.putExtra("LONGITUDE",landmarks.get(position).getLocationLongitude());
+        intent.putExtra("LATITUDE",landmarks.get(position).getLocationLatitude());
+        context.startActivity(intent);
+    }
 }
